d2450cc0fb6b65903751fa6662f25c8c51b83731
Anderson Almanzar <openedspaces@gmail.com>
2026-07-13 20:13:22 -0400

    Consolidate duplicate serverless functions into existing catch-all

--- api/auth/logout.js
+++
@@ -1,8 +0,0 @@
-// api/auth/logout.js -> POST /api/auth/logout
-const authService = require('../../lib/authService');
-const { withHandler } = require('../../lib/httpHelpers');
-
-module.exports = withHandler(async (req, res) => {
-  const result = await authService.logout();
-  res.status(200).json(result);
-});
--- api/content/languages.js
+++
@@ -1,6 +0,0 @@
-const { levelContent, languageContent } = require('../../lib/uiContent');
-const { withHandler } = require('../../lib/httpHelpers');
-
-module.exports = withHandler(async (_req, res) => {
-  res.status(200).json({ levelContent, languageContent });
-});
